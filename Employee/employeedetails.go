ab940431daa00d25ad1c852e9da7794e9e7771f8
amitramesh <amit.ramesh.sharma@gmail.com>
2017-01-08 17:02:53 +0530

    added new structure in employee file

--- Employee/employeedetails.go
+++ Employee/employeedetails.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main(){
 	fmt.Println("This is my employee class which i have created in webstrom")
+	s:= Employee{name:"Amit Sharma",age:20}
+	fmt.Println(s.name)
+	fmt.Println( s.age )
+}
+
+type  Employee struct{
+
+	name string
+	age int
 }
